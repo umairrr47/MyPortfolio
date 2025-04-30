c77b2484aed6d4c39803dd2e440e389e9d7de2d7
Mohammad Umair <143015748+umairrr47@users.noreply.github.com>
2025-04-30 21:18:20 +0530

    changes

--- src/components/footer/FooterMain.jsx
+++ src/components/footer/FooterMain.jsx
@@ -44,7 +44,7 @@ const FooterMain = () => {
         </ul>
       </div>
       <p className="max-w-[1200px] mx-auto text-right mt-2 mb-12 text-sm text-lightBrown">
-        © 2024 Umair | All Rights Reserved.
+        © 2025 Umair | All Rights Reserved.
       </p>
     </div>
   );
